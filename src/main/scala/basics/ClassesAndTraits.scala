4caabfa5c4f38fc58efa12de3fad93f621b62c4d
vpletuhin <qwertyzmail@gmail.com>
2021-01-31 20:27:59 +0300

    classes and traits homework

--- src/main/scala/basics/ClassesAndTraits.scala
+++ src/main/scala/basics/ClassesAndTraits.scala
@@ -18,8 +18,9 @@ object ClassesAndTraits {
     def maxY: Double
   }
 
-  sealed trait Movable[T] {
-    def move(dx: Double, dy: Double): T
+  sealed trait Movable[+S] {
+    this: S =>
+    def move(dx: Double, dy: Double): S
   }
 
   final case class Point(x: Double, y: Double) extends Shape with Movable[Point] {
@@ -43,16 +44,21 @@ object ClassesAndTraits {
     override def area: Double = (maxX - minX) * (maxY - minY)
   }
 
-  sealed trait RectangleMovable[T] extends RectangleBase with Movable[T] {
-    def at(p: Point): T
-    override def move(dx: Double, dy: Double): T = at(Point(this.x, this.y))
+  sealed trait RectangleMovable[+S] extends RectangleBase with Movable[S] {
+    this: S =>
+    def at(p: Point): S
+    override def move(dx: Double, dy: Double): S = at(Point(this.x + dx, this.y + dy))
   }
 
-  final case class Rectangle(center: Point, width: Double, height: Double) extends RectangleMovable[Rectangle] {
+  final case class Rectangle(center: Point, width: Double, height: Double)
+    extends RectangleBase(center, width, height)
+      with RectangleMovable[Rectangle] {
     override def at(p: Point): Rectangle = Rectangle(p, width, height)
   }
 
-  final case class Square(center: Point, side: Double) extends RectangleMovable[Square] {
+  final case class Square(center: Point, side: Double)
+    extends RectangleBase(center, side, side)
+      with RectangleMovable[Square] {
     override def at(p: Point): Square = Square(p, side)
   }
 
@@ -67,6 +73,7 @@ object ClassesAndTraits {
     override def move(dx: Double, dy: Double): Triangle = Triangle.tupled(
       List(p1, p2, p3).map(_.move(dx, dy)) match {
         case p1 :: p2 :: p3 :: Nil => (p1, p2, p3)
+        case _ => throw new RuntimeException
       })
 
     override def area: Double = {
@@ -87,8 +94,9 @@ object ClassesAndTraits {
     def volume: Double
   }
 
-  sealed trait Movable3D[T] {
-    def move(dx: Double, dy: Double, dz: Double): T
+  sealed trait Movable3D[+S] {
+    this: S =>
+    def move(dx: Double, dy: Double, dz: Double): S
   }
 
   case class Origin3D() extends Shape3D {
@@ -102,24 +110,33 @@ object ClassesAndTraits {
     override def move(dx: Double, dy: Double, dz: Double): Point3D = Point3D(x + dx, y + dy, z + dz)
   }
 
-  case class Sphere(center: Point3D, radius: Double) extends Shape3D {
+  case class Sphere(center: Point3D, radius: Double) extends Shape3D with Movable3D[Sphere] {
     override def surfaceArea: Double = ???
     override def volume: Double = ???
+    override def move(dx: Double, dy: Double, dz: Double): Sphere = Sphere(center.move(dx, dy, dz), radius)
   }
 
-  case class Cube(center: Point3D, sideLen: Double) extends Shape3D {
+  case class Cube(center: Point3D, side: Double) extends Shape3D with Movable3D[Cube] {
     override def surfaceArea: Double = ???
     override def volume: Double = ???
+    override def move(dx: Double, dy: Double, dz: Double): Cube = Cube(center.move(dx, dy, dz), side)
   }
 
-  case class Cuboid(center: Point3D, width: Double, height: Double) extends Shape3D {
+  case class Cuboid(center: Point3D, width: Double, height: Double) extends Shape3D with Movable3D[Cuboid] {
     override def surfaceArea: Double = ???
     override def volume: Double = ???
+    override def move(dx: Double, dy: Double, dz: Double): Cuboid = Cuboid(center.move(dx, dy, dz), width, height)
   }
 
-  case class Triangle3D(p1: Point3D, p2: Point3D, p3: Point, p4: Point) extends Shape3D {
+  case class Triangle3D(p1: Point3D, p2: Point3D, p3: Point3D, p4: Point3D) extends Shape3D with Movable3D[Triangle3D] {
     override def surfaceArea: Double = ???
     override def volume: Double = ???
+    // much fun again
+    override def move(dx: Double, dy: Double, dz: Double): Triangle3D = Triangle3D.tupled(
+      List(p1, p2, p3, p4).map(_.move(dx, dy, dz)) match {
+        case p1 :: p2 :: p3 :: p4 :: Nil => (p1, p2, p3, p4)
+        case _ => throw new RuntimeException
+      })
   }
 
 }
