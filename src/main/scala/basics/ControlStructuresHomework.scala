0e73474c7c204b02856618e27bfb9470c352bdf2
vpletuhin <qwertyzmail@gmail.com>
2021-02-04 16:21:04 +0300

    control structures homework

---
+++ src/main/scala/basics/ControlStructuresHomework.scala
@@ -0,0 +1,73 @@
+package basics
+
+import scala.io.Source
+import scala.util.Try
+
+object ControlStructuresHomework {
+
+  sealed trait Command
+  object Command {
+    final case class Divide(dividend: Double, divisor: Double) extends Command
+    final case class Sum(numbers: List[Double]) extends Command
+    final case class Average(numbers: List[Double]) extends Command
+    final case class Min(numbers: List[Double]) extends Command
+    final case class Max(numbers: List[Double]) extends Command
+  }
+
+  final case class ErrorMessage(value: String)
+  final case class Result(cmd: Command, value: Double)
+
+  import Command._
+
+  def parseCommand(x: String): Either[ErrorMessage, Command] = {
+    def parseNumbers(in: List[String]): Either[ErrorMessage, List[Double]] = Try(in.map(_.toDouble)).toOption.toRight(ErrorMessage("wrong numbers format"))
+
+    x.split(' ').toList match {
+      case Nil | _ :: Nil => Left(ErrorMessage("nothing to calculate"))
+      case cmd :: nums =>
+        parseNumbers(nums).flatMap(numbers => cmd match {
+          case "divide" => Either.cond(numbers.length == 2, Divide(numbers.head, numbers.last), ErrorMessage("divide command takes 2 numbers"))
+          case "sum" => Right(Sum(numbers))
+          case "average" => Right(Average(numbers))
+          case "min" => Right(Min(numbers))
+          case "max" => Right(Max(numbers))
+          case _ => Left(ErrorMessage("unknown command"))
+        })
+    }
+  }
+
+  def calculate(x: Command): Either[ErrorMessage, Result] = x match {
+    case Divide(dividend, divisor) => Either.cond(divisor != 0, Result(x, dividend / divisor), ErrorMessage("/ by 0"))
+    case Sum(numbers) => Right(Result(x, numbers.sum))
+    case Average(numbers) => Right(Result(x, numbers.sum / numbers.length))
+    case Min(numbers) => Right(Result(x, numbers.min))
+    case Max(numbers) => Right(Result(x, numbers.max))
+  }
+
+  def renderResult(x: Result): String = x.cmd match {
+    case Divide(dividend, divisor) => s"${mkString(dividend)} divided by ${mkString(divisor)} is ${mkString(x.value)}"
+    case Sum(numbers) => render("sum", numbers, x.value)
+    case Average(numbers) => render("average", numbers, x.value)
+    case Min(numbers) => render("minimum", numbers, x.value)
+    case Max(numbers) => render("maximum", numbers, x.value)
+  }
+
+  private def render(cmdName: String, numbers: List[Double], value: Double): String = {
+    s"the $cmdName of ${mkString(numbers: _*)} is ${mkString(value)}"
+  }
+
+  private def mkString(values: Double*): String = values.map { value =>
+    new java.math.BigDecimal(value.toString).stripTrailingZeros().toPlainString
+  }.mkString(" ")
+
+  def process(x: String): String = {
+    val result = for {
+      cmd <- parseCommand(x)
+      r <- calculate(cmd)
+    } yield renderResult(r)
+
+    result.fold("Error: " + _.value, identity)
+  }
+
+  def main(args: Array[String]): Unit = Source.stdin.getLines() map process foreach println
+}
---
+++ src/test/scala/basics/ControlStructuresHomeworkTest.scala
@@ -0,0 +1,50 @@
+package basics
+
+import basics.ControlStructuresHomework._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+class ControlStructuresHomeworkTest extends AnyFlatSpec with should.Matchers {
+
+  "sum" should "calc correctly" in {
+    process("sum 5 5 6 8.5") should be("the sum of 5 5 6 8.5 is 24.5")
+  }
+
+  "average" should "calc correctly" in {
+    process("average 4 3 8.5 4") should be("the average of 4 3 8.5 4 is 4.875")
+  }
+
+  "divide" should "calc correctly" in {
+    process("divide 4 5") should be("4 divided by 5 is 0.8")
+  }
+
+  "min" should "calc correctly" in {
+    process("min 4 -3 -17") should be("the minimum of 4 -3 -17 is -17")
+  }
+
+  "max" should "calc correctly" in {
+    process("max 4 -3 -17") should be("the maximum of 4 -3 -17 is 4")
+  }
+
+  "divide by 0" should "return error" in {
+    process("divide 1 0") should be("Error: / by 0")
+  }
+
+  "divide with more or less than 2 numbers" should "return error" in {
+    process("divide 1 2 3") should be("Error: divide command takes 2 numbers")
+    process("divide 1") should be("Error: divide command takes 2 numbers")
+  }
+
+  "unknown command" should "return error" in {
+    process("ln 0") should be("Error: unknown command")
+  }
+
+  "if no numbers" should "return error" in {
+    process("sum") should be("Error: nothing to calculate")
+  }
+
+  "empty string" should "return error" in {
+    process("") should be("Error: nothing to calculate")
+  }
+
+}
