bcd76f8c2160fcadd3d1d3cf3f68cfa74659757b
vpletuhin <qwertyzmail@gmail.com>
2021-04-11 13:41:24 +0300

    shared state homework

--- src/main/scala/effects/SharedStateHomework.scala
+++ src/main/scala/effects/SharedStateHomework.scala
@@ -5,6 +5,7 @@ import cats.effect._
 import cats.effect.concurrent.Ref
 import cats.implicits._
 
+import scala.concurrent.duration
 import scala.concurrent.duration._
 
 /*
@@ -21,58 +22,59 @@ object SharedStateHomework extends IOApp {
     def put(key: K, value: V): F[Unit]
   }
 
+  type CacheData[K, V] = Map[K, (Long, V)]
+  type CacheEntry[K, V] = (K, (Long, V))
   type CacheState[F[_], K, V] = Ref[F, Map[K, (Long, V)]]
 
-  class RefCache[F[_] : Clock : Monad, K, V](state: Ref[F, Map[K, (Long, V)]], expiresIn: FiniteDuration) extends Cache[F, K, V] {
+  class RefCache[F[_] : Clock : Monad, K, V](state: CacheState[F, K, V], expiresIn: FiniteDuration) extends Cache[F, K, V] {
     def get(key: K): F[Option[V]] = {
-      state.modify { values =>
-        val value = values.get(key)
-        val expired = value.exists {
-          case (created, _) =>
-            val lifetime = System.currentTimeMillis() - created
-            lifetime.millis > expiresIn
+      val ifNotExpired = (time: Long) => state.modify { map =>
+        map.get(key) match {
+          case Some(entry) if expired(time, (key, entry), expiresIn) => (map.removed(key), None)
+          case Some((_, value)) => (map, Some(value))
+          case None => (map, None)
         }
-        val newState = if (expired) values.removed(key) else values
-        val result = value.filterNot(_ => expired).map(_._2)
-        (newState, result)
       }
+
+      time >>= ifNotExpired
     }
 
     def put(key: K, value: V): F[Unit] = {
-      for {
-        time <- time()
-        state <- state.getAndUpdate(_ + (key -> (time, value)))
-      } yield state.void
-    }
+      val put = (entry: CacheEntry[K, V]) => state.update(_ + entry)
+      val entry = (key: K, value: V) => (time: Long) => (key -> (time, value)).pure[F]
 
-    private def time(): F[Long] = Clock[F].realTime(expiresIn.unit)
+      time >>= entry(key, value) >>= put
+    }
   }
 
   object Cache {
     def of[F[_] : Clock, K, V](expiresIn: FiniteDuration, checkOnExpirationsEvery: FiniteDuration)
                               (implicit T: Timer[F], C: Concurrent[F]): F[Cache[F, K, V]] = {
-
       for {
         state <- Ref.of(Map.empty[K, (Long, V)])
-        exp <- expiring(state, expiresIn, checkOnExpirationsEvery)
-        ref = new RefCache[F, K, V](exp, expiresIn)
+        _ <- C.start(expiring(state, expiresIn, checkOnExpirationsEvery))
+        ref = new RefCache[F, K, V](state, expiresIn)
       } yield ref
     }
 
     def expiring[F[_] : Clock, K, V](state: CacheState[F, K, V], expiresIn: FiniteDuration, checkOnExpirationsEvery: FiniteDuration)
-                     (implicit T: Timer[F], C: Concurrent[F]): F[CacheState[F, K, V]] = {
-      val clean = (time: Long) => state.getAndUpdate { values =>
-        values.filter {
-          case (_, (created, _)) => (time - created).millis < expiresIn
-        }
-      }.as(state)
+                                    (implicit T: Timer[F], C: Concurrent[F]): F[Unit] = {
+      val task = for {
+        _ <- T.sleep(checkOnExpirationsEvery)
+        time <- time
+        _ <- state.update(_.filterNot(expired(time, _, expiresIn)))
+      } yield ()
 
-      C.defer(Clock[F].realTime(expiresIn.unit))
-        .flatMap(clean)
-        .productL(T.sleep(checkOnExpirationsEvery))
-        .foreverM
+      task.foreverM
     }
   }
+
+  private def expired[F[_], K, V](time: Long, entry: CacheEntry[K, V], expiresIn: FiniteDuration): Boolean = {
+    val (_, (created, _)) = entry
+    (time - created).millis > expiresIn
+  }
+
+  private def time[F[_] : Clock]: F[Long] = Clock[F].realTime(duration.MILLISECONDS)
 
   override def run(args: List[String]): IO[ExitCode] = {
     for {
