d206be5d685ba3f3f9c46b0dfe5325bd89cc1353
vpletuhin <qwertyzmail@gmail.com>
2021-05-06 19:04:34 +0300

    effects additional tasks

---
+++ src/main/scala/effects/Race.scala
@@ -0,0 +1,59 @@
+package effects
+
+import cats.data._
+import cats.effect.{ExitCase, ExitCode, IO, IOApp, Timer}
+
+import scala.concurrent.duration._
+import scala.util.Random
+
+object Race extends IOApp {
+  case class Data(source: String, body: String)
+
+  def provider(name: String)(implicit timer: Timer[IO]): IO[Data] = {
+    val proc = for {
+      dur <- IO {
+        Random.nextInt(500)
+      }
+      _ <- IO.sleep {
+        (100 + dur).millis
+      }
+      _ <- IO {
+        if (Random.nextBoolean()) throw new Exception(s"Error in $name")
+      }
+      txt <- IO {
+        Random.alphanumeric.take(16).mkString
+      }
+    } yield Data(name, txt)
+
+    proc.guaranteeCase {
+      case ExitCase.Completed => IO {
+        println(s"$name request finished")
+      }
+      case ExitCase.Canceled => IO {
+        println(s"$name request canceled")
+      }
+      case ExitCase.Error(ex) => IO {
+        println(s"$name errored")
+      }
+    }
+  }
+
+  // Use this class for reporting all failures.
+  case class CompositeException(ex: NonEmptyList[Throwable]) extends Exception("All race candidates have failed")
+
+  // Implement this function:
+  def raceToSuccess[A](ios: NonEmptyList[IO[A]]): IO[A] = ???
+
+  // In your IOApp, you can use the following sample method list
+
+  val methods: NonEmptyList[IO[Data]] = NonEmptyList.of(
+    "memcached",
+    "redis",
+    "postgres",
+    "mongodb",
+    "hdd",
+    "aws"
+  ).map(provider)
+
+  override def run(args: List[String]): IO[ExitCode] = ???
+}
---
+++ src/main/scala/effects/WorkerPoolExcercise.scala
@@ -0,0 +1,76 @@
+package effects
+
+import cats.effect._
+import cats.effect.concurrent.Ref
+import cats.implicits._
+
+import java.util.concurrent.{ConcurrentLinkedQueue, Executors}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.util.Random
+
+object WorkerPoolExcercise extends IOApp {
+  // To start, our requests can be modelled as simple functions.
+  // You might want to replace this type with a class if you go for bonuses. Or not.
+  type Worker[A, B] = A => IO[B]
+
+  // Sample stateful worker that keeps count of requests it has accepted
+  def mkWorker(id: Int)(implicit timer: Timer[IO]): IO[Worker[Int, Int]] =
+    Ref[IO].of(0).map { counter =>
+      def simulateWork: IO[Unit] =
+        IO(println(s"work started by $id")) >> IO(50 + Random.nextInt(450)).map(_.millis).flatMap(IO.sleep)
+
+      def report: IO[Unit] =
+        counter.get.flatMap(i => IO(println(s"Total processed by $id: $i")))
+
+      x =>
+        simulateWork >>
+          counter.update(_ + 1) >>
+          report >>
+          IO.pure(x + 1)
+    }
+
+  trait WorkerPool[A, B] {
+    def exec(a: A): IO[B]
+  }
+
+  object WorkerPool {
+
+    // Implement this constructor, and, correspondingly, the interface above.
+    // You are free to use named or anonymous classes
+    def of[A, B](fs: List[Worker[A, B]]): IO[WorkerPool[A, B]] = {
+      val ec = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+      val be = Blocker.liftExecutionContext(ec)
+
+      val freeWorkers = new ConcurrentLinkedQueue[Worker[A, B]]()
+      fs.foreach(w => freeWorkers.add(w))
+
+      def worker(): Resource[IO, Worker[A, B]] = {
+        val acquire = Concurrent[IO].untilDefinedM(IO(Option(freeWorkers.poll())))
+        val release = (w: Worker[A, B]) => IO(freeWorkers.add(w))
+        Resource.make(acquire <* IO(println("worker obtained")))(release(_) *> IO(println("returning worker")))
+      }
+
+      IO { a =>
+        be.blockOn {
+          worker().use(_.apply(a))
+        }
+      }
+    }
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val testPool: IO[WorkerPool[Int, Int]] =
+      List.range(0, 10)
+        .traverse(mkWorker)
+        .flatMap(WorkerPool.of)
+
+    for {
+      pool <- testPool
+      nums = (0 to 100).toList
+      fibers <- nums.traverse(num => pool.exec(num).start)
+      result <- fibers.traverse(_.join)
+      _ <- IO(println(result))
+    } yield ExitCode.Success
+  }
+}
